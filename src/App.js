4ca0be9a5115087d4705a535a8b4467d62ac3158
EduardoZola <eduzol@gmail.com>
2017-08-21 02:35:26 -0700

    Book Grid Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import Book from './Book'
+import BookGrid from './BookGrid'
 import './App.css'
-/**
- * 
-BooksAPI.search('iOS', 10).then(function(data){
-  console.log('data ', data);
-});
- */
 
 class BooksApp extends React.Component {
   state = {
@@ -17,7 +11,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true
+    showSearchPage: true, 
+    currentlyReadingBooks : []
+  }
+
+  componentDidMount(){
+    BooksAPI.search('iOS' , 3).then((data) => {
+      console.log('data ' , data );
+      this.setState({currentlyReadingBooks : data } );
+    });
   }
 
   render() {
@@ -54,29 +56,7 @@ class BooksApp extends React.Component {
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                       <Book author="Author" title="Title" coverUrl='http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api'  />
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
+                    <BookGrid data={this.state.currentlyReadingBooks} />
                   </div>
                 </div>
                 <div className="bookshelf">
