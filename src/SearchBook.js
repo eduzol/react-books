2d1fff3d9cd817c5614a7f8cb39054c93053312f
EduardoZola <eduzol@gmail.com>
2017-08-28 11:24:36 -0700

    remove comments

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -9,9 +9,7 @@ class SearchBook extends Component {
                 <div className="search-books-bar">
                 <Link to="/" className="close-search" >Close</Link>
                 <div className="search-books-input-wrapper">
-                  
-                    <input type="text" placeholder="Search by title or author"/>
-                    
+                  <input type="text" placeholder="Search by title or author"/>
                 </div>
                 </div>
                 <div className="search-books-results">
